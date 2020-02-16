6056c63892825b02a89e6c35077dcc3e4cd63a1b
TomboY90 <rjsahdia3@gmail.com>
2020-02-16 09:36:28 +0100

    Add mongoDB Connect

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect DataBase
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
