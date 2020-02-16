d2110476120253f9a5acfc571168849d56d4b6f6
TomboY90 <rjsahdia3@gmail.com>
2020-02-16 10:02:48 +0100

    Add user validation

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // Connect DataBase
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
